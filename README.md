97152bf93891cf4fc12579631f5c5fcc143d9303
Amp&rsand <113919662+MuriloSchneiderS@users.noreply.github.com>
2025-10-06 20:07:53 -0300

    organização e criação dos arquivos iniciais

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # IFSC-WEB1-AV2
 Avaliação 2 do curso de web1 em ADS no IFSC.
+
+## Contextualização 
+ 
+Dashboard interativo de cotação de moeda, 
+a aplicação tem como funcionalidades principais: 
+●  A conversão de moedas em tempo real 
+●  Uma tabela de cotações mostrando o valor de várias moedas em relação a moeda
---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+/*Conversão de moedas: 
+1.  Um input para seleção de moeda, junto com 2 selects de seleção de moeda destino 
+e moeda final. 
+ 
+2.  Os menus de seleção de moeda devem ser carregados dinamicamente junto com a 
+página por uma requisição à API. 
+ 
+3.  Ao alterar o valor de qualquer um dos campos, o valor deve ser atualizado 
+automaticamente. 
+ 
+4.  Durante o processo de requisição, deve estar presente um ícone de carregamento 
+ou mensagem de carregamento. 
+ 
+Tabela de Cotações: 
+1.  Abaixo do conversor de moedas deve haver uma tabela de cotações das moedas 
+mais importantes em relação ao real brasilieiro, fica a seu critério quais moedas 
+comparar. 
+ 
+2.  A tabela deve ser gerada ao carregar a página de forma dinâmica 
+ 
+3.  Deve haver um botão logo abaixo para atualizar as cotas que deve buscar os dados 
+mais recentes e atualizar apropriadamente. 
+ 
+Extras: 
+ 
+1.  Previna erros possíveis da API, para caso ocorra erros de busca de dados o site se 
+comportar devidamente com mensagens personalizadas para diferentes problemas. 
+ 
+2.  Previna que o usuário coloque valores inválidos no campo de moeda, notificando o 
+mesmo quando isso acontecer. 
+ 
+ 
+Links para requisições: 
+●  https://api.frankfurter.app */
